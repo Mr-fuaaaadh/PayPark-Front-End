342405acec1cca8b2dd9bd2c1a741384847d4f29
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-03-10 07:27:10 +0100

    eb

--- src/components/listing/sidebar/PriceRange.js
+++ src/components/listing/sidebar/PriceRange.js
@@ -1,33 +1,36 @@
 "use client";
 import React, { useState } from "react";
-import Slider, { Range } from "rc-slider";
-import "rc-slider/assets/index.css";
+import InputRange from "react-input-range";
+import "react-input-range/lib/css/index.css";
 
-const PriceRange = ({ filterFunctions }) => {
-  const [price, setPrice] = useState([20, 70987]);
+const PriceRange = ({filterFunctions}) => {
+  const [price, setPrice] = useState({ value: { min: 20, max: 70987 } });
 
   // price range handler
   const handleOnChange = (value) => {
-    setPrice(value);
-    filterFunctions?.handlepriceRange(value);
-  };
+    setPrice({ value });
+ 
+  filterFunctions?.handlepriceRange([value.min  || 0,value.max])
+ };
 
   return (
-    <div className="range-wrapper">
-      <Range
-        min={0}
-        max={100000}
-        value={filterFunctions?.priceRange || price}
-        onChange={handleOnChange}
-        id="slider"
-        allowCross={false}
-      />
-      <div className="d-flex align-items-center">
-        <span id="slider-range-value1">${price[0]}</span>
-        <i className="fa-sharp fa-solid fa-minus mx-2 dark-color icon" />
-        <span id="slider-range-value2">${price[1]}</span>
+    <>
+      <div className="range-wrapper">
+        <InputRange
+          formatLabel={() => ``}
+          maxValue={100000}
+          minValue={0}
+          value={{min:filterFunctions?.priceRange[0],max:filterFunctions?.priceRange[1]}}
+          onChange={(value) => handleOnChange(value)}
+          id="slider"
+        />
+        <div className="d-flex align-items-center">
+          <span id="slider-range-value1">${price.value.min}</span>
+          <i className="fa-sharp fa-solid fa-minus mx-2 dark-color icon" />
+          <span id="slider-range-value2">${price.value.max}</span>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/pages/_document.js
+++ src/components/pages/_document.js
@@ -1,15 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Place your beforeInteractive scripts here */}
-        <Script
-          src="https://your-script-url.js"
-          strategy="beforeInteractive"
-        />
+        {/* Move next/script components here if using beforeInteractive */}
       </Head>
       <body>
         <Main />
