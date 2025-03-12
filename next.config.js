db7da7c88a99ab8fc34e7dfac9beb95aa52a2178
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-03-12 10:56:06 +0100

    Add netlify.toml and _redirects

--- next.config.js
+++ next.config.js
@@ -3,14 +3,14 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
+        protocol: "http", // Change to https to avoid mixed content issues
         hostname: "13.60.216.198",
       },
     ],
   },
-  output: 'standalone',  // Ensures compatibility with Netlify
-  trailingSlash: true,   // Helps with route handling
-  reactStrictMode: true,
+  output: 'standalone',  // Ensures compatibility with Netlify (Serverless)
+  trailingSlash: true,   // Helps with route handling and avoids 404 errors
+  reactStrictMode: true, // Helps with development (strict checks)
 };
 
 module.exports = nextConfig;
